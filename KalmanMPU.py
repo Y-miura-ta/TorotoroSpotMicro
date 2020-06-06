5121a6213b5df5722b6ddd305a15338a3666da8f
miura <pi@spotmicro>
2020-06-07 01:22:19 +0900

    カルマンフィルタのタイマ割り込み未実装

--- KalmanMPU.py
+++ KalmanMPU.py
@@ -2,6 +2,7 @@
 
 from math import *
 import smbus
+import time
 import Kalman as kf
 
 # some MPU6050 Registers and their Address
@@ -74,75 +75,62 @@ gyroXangle = roll
 gyroYangle = pitch
 compAngleX = roll
 compAngleY = pitch
+kalAngleX = roll
+kalAngleY = pitch
 
-def kalmanLoop():
+# タイマ割り込みの周期[s]
+dt = 1.0e-4
+
+def kalmanLoop(kalAngleX, kalAngleY, kalmanX, kalmanY, kalmanZ, gyroXangle, gyroYangle, compAngleX, compAngleY):
     # Update all the values
     accX = read_raw_data(ACCEL_XOUT_H)
     accY = read_raw_data(ACCEL_YOUT_H)
     accZ = read_raw_data(ACCEL_ZOUT_H)
-    tempRaw = read_raw_data()
-#   gyroX = (int16_t)((i2cData[8] << 8) | i2cData[9]);
-#   gyroY = (int16_t)((i2cData[10] << 8) | i2cData[11]);
-#   gyroZ = (int16_t)((i2cData[12] << 8) | i2cData[13]);;
+    tempRaw = read_raw_data(TEMP_OUT_H)
+    gyroX = read_raw_data(GYRO_XOUT_H)
+    gyroY = read_raw_data(GYRO_YOUT_H)
+    gyroZ = read_raw_data(GYRO_ZOUT_H)
 
-#   double dt = (double)(micros() - timer) / 1000000; // Calculate delta time
-#   timer = micros();
+    roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG
+    pitch = atan2(-accX, accZ) * RAD_TO_DEG
 
-#   // Source: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf eq. 25 and eq. 26
-#   // atan2 outputs the value of -π to π (radians) - see http://en.wikipedia.org/wiki/Atan2
-#   // It is then converted from radians to degrees
-# #ifdef RESTRICT_PITCH // Eq. 25 and 26
-#   double roll  = atan2(accY, accZ) * RAD_TO_DEG;
-#   double pitch = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
-# #else // Eq. 28 and 29
-#   double roll  = atan(accY / sqrt(accX * accX + accZ * accZ)) * RAD_TO_DEG;
-#   double pitch = atan2(-accX, accZ) * RAD_TO_DEG;
-# #endif
+    gyroXrate = gyroX / 131.0 # Convert to deg/s
+    gyroYrate = gyroY / 131.0 # Convert to deg/s
 
-#   double gyroXrate = gyroX / 131.0; // Convert to deg/s
-#   double gyroYrate = gyroY / 131.0; // Convert to deg/s
+    # This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
+    if((pitch < -90 and kalAngleY > 90) or (pitch > 90 and kalAngleY < -90)):
+        kalmanY.setAngle(pitch)
+        compAngleY = pitch
+        kalAngleY = pitch
+        gyroYangle = pitch
+    else:
+        kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt) # Calculate the angle using a Kalman filter
 
-# #ifdef RESTRICT_PITCH
-#   // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-#   if ((roll < -90 && kalAngleX > 90) || (roll > 90 && kalAngleX < -90)) {
-#     kalmanX.setAngle(roll);
-#     compAngleX = roll;
-#     kalAngleX = roll;
-#     gyroXangle = roll;
-#   } else
-#     kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
+    if(abs(kalAngleY) > 90):
+        gyroXrate = -gyroXrate # Invert rate, so it fits the restriced accelerometer reading
+        
+    kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt) # Calculate the angle using a Kalman filter
 
-#   if (abs(kalAngleX) > 90)
-#     gyroYrate = -gyroYrate; // Invert rate, so it fits the restriced accelerometer reading
-#   kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt);
-# #else
-#   // This fixes the transition problem when the accelerometer angle jumps between -180 and 180 degrees
-#   if ((pitch < -90 && kalAngleY > 90) || (pitch > 90 && kalAngleY < -90)) {
-#     kalmanY.setAngle(pitch);
-#     compAngleY = pitch;
-#     kalAngleY = pitch;
-#     gyroYangle = pitch;
-#   } else
-#     kalAngleY = kalmanY.getAngle(pitch, gyroYrate, dt); // Calculate the angle using a Kalman filter
+    gyroXangle += gyroXrate * dt # Calculate gyro angle without any filter
+    gyroYangle += gyroYrate * dt
+    # gyroXangle += kalmanX.getRate() * dt # Calculate gyro angle using the unbiased rate
+    # gyroYangle += kalmanY.getRate() * dt
 
-#   if (abs(kalAngleY) > 90)
-#     gyroXrate = -gyroXrate; // Invert rate, so it fits the restriced accelerometer reading
-#   kalAngleX = kalmanX.getAngle(roll, gyroXrate, dt); // Calculate the angle using a Kalman filter
-# #endif
+    compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll # Calculate the angle using a Complimentary filter
+    compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch
 
-#   gyroXangle += gyroXrate * dt; // Calculate gyro angle without any filter
-#   gyroYangle += gyroYrate * dt;
-#   //gyroXangle += kalmanX.getRate() * dt; // Calculate gyro angle using the unbiased rate
-#   //gyroYangle += kalmanY.getRate() * dt;
+    # Reset the gyro angle when it has drifted too much
+    if (gyroXangle < -180 or gyroXangle > 180):
+        gyroXangle = kalAngleX
+    if (gyroYangle < -180 or gyroYangle > 180):
+        gyroYangle = kalAngleY
 
-#   compAngleX = 0.93 * (compAngleX + gyroXrate * dt) + 0.07 * roll; // Calculate the angle using a Complimentary filter
-#   compAngleY = 0.93 * (compAngleY + gyroYrate * dt) + 0.07 * pitch;
+    print("pitch:{}, gy_pitch:{}, cmp_pitch:{}, kal_pitch:{}".format(pitch, gyroYangle, compAngleY, kalAngleY))
 
-#   // Reset the gyro angle when it has drifted too much
-#   if (gyroXangle < -180 || gyroXangle > 180)
-#     gyroXangle = kalAngleX;
-#   if (gyroYangle < -180 || gyroYangle > 180)
-#     gyroYangle = kalAngleY;
+    return kalAngleX, kalAngleY, kalmanX, kalmanY, kalmanZ, gyroXangle, gyroYangle, compAngleX, compAngleY
+
+while(1):
+    kalAngleX, kalAngleY, kalmanX, kalmanY, kalmanZ, gyroXangle, gyroYangle, compAngleX, compAngleY = kalmanLoop(kalAngleX, kalAngleY, kalmanX, kalmanY, kalmanZ, gyroXangle, gyroYangle, compAngleX, compAngleY)
 
 #   /* Print Data */
 # #if 0 // Set to 1 to activate
